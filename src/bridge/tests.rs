e0ebc941e129d7d286af79e06826d50047031065
d3npa <d3npa@pm.me>
2021-02-27 19:59:01 -0500

    Added tests for errors

--- src/bridge/tests.rs
+++ src/bridge/tests.rs
@@ -1,6 +1,6 @@
 use crate::bridge::*;
 use std::net::{IpAddr, Ipv4Addr};
-use std::ffi::CString;
+use std::convert::TryInto;
 
 #[test]
 fn convert_pfaddr_from_c() {
@@ -16,6 +16,12 @@ fn convert_pfaddr_from_c() {
     assert_eq!(pf_addr.addr, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)));
     assert_eq!(pf_addr.ifname, String::from("vio0"));
     assert_eq!(pf_addr.subnet, 32);
+}
+
+#[test]
+fn convert_pfaddr_from_c_fail_af() {
+    let c_addr = pfr_addr::init();
+    assert!(PfAddr::try_from(c_addr).is_err());
 }
 
 #[test]
@@ -37,4 +43,15 @@ fn convert_pfaddr_into_c() {
         [118, 105, 111, 48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     );
     assert_eq!(c_addr.pfra_net, 32);
+}
+
+#[test]
+fn convert_pfaddr_into_c_fail_len() {
+    let pf_addr = PfAddr {
+        addr: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+        ifname: String::from_utf8(vec![61u8; 16]).unwrap(),
+        subnet: 32,
+    };
+
+    assert!(TryInto::<pfr_addr>::try_into(pf_addr).is_err());
 }
