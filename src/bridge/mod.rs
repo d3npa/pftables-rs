1403301df956db20852c6f802035326f3c1dd005
d3npa <d3npa@pm.me>
2021-03-01 12:24:45 -0500

    Add Rustic structs for PfrTable and PfiocTable and conversion implementations

--- src/bridge/mod.rs
+++ src/bridge/mod.rs
@@ -6,17 +6,18 @@ use std::convert::{TryFrom, TryInto};
 use crate::PfError;
 
 // Create more Rust-friendly (and safer) versions of the pf structs
-pub struct PfAddr {
+#[derive(Debug, PartialEq)]
+pub struct PfrAddr {
     pub addr: IpAddr,
     pub ifname: String,
     pub subnet: u8,
     // Other fields are unused right now
 }
 
-impl TryFrom<pfr_addr> for PfAddr {
+impl TryFrom<pfr_addr> for PfrAddr {
     type Error = crate::PfError;
-    /// Will fail if pfra_af field is invalid
-    fn try_from(a: pfr_addr) -> Result<PfAddr, PfError> {
+    /// Will fail if pfra_af field is invalid or if pfra_ifname contains invalid utf8
+    fn try_from(a: pfr_addr) -> Result<PfrAddr, PfError> {
         let addr = match a.pfra_af {
             AF_INET => {
                 let v = unsafe { a.pfra_u._pfra_ip4addr };
@@ -38,10 +39,10 @@ impl TryFrom<pfr_addr> for PfAddr {
 
         let ifname = match String::from_utf8(ifname) {
             Ok(v) => v,
-            Err(_) => return Err(PfError::ConversionError)
+            Err(_) => return Err(PfError::ConversionError),
         };
 
-        Ok(PfAddr {
+        Ok(PfrAddr {
             addr,
             ifname,
             subnet: a.pfra_net,
@@ -49,9 +50,9 @@ impl TryFrom<pfr_addr> for PfAddr {
     }
 }
 
-impl TryInto<pfr_addr> for PfAddr {
+impl TryInto<pfr_addr> for PfrAddr {
     type Error = crate::PfError;
-    /// Will fail if ifname is 16 or greater
+    /// Will fail if ifname is IFNAMSIZ characters or longer
     fn try_into(self) -> Result<pfr_addr, PfError> {
         if self.ifname.len() >= IFNAMSIZ {
             return Err(PfError::ConversionError);
@@ -77,4 +78,123 @@ impl TryInto<pfr_addr> for PfAddr {
 
         Ok(c_addr)
     }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct PfrTable {
+    pub anchor: String,
+    pub name: String,
+    // Fields below are unused at the moment
+    // pub flags: u32,
+    // pub fback: u8,
+}
+
+impl TryFrom<pfr_table> for PfrTable {
+    type Error = crate::PfError;
+    /// Will fail if pfrt_anchor or pfrt_name contain invalid utf8
+    fn try_from(t: pfr_table) -> Result<PfrTable, PfError> {
+        let mut anchor = t.pfrt_anchor.to_vec();
+        while anchor.len() > 0 && anchor[anchor.len() - 1] == 0 {
+            anchor.pop();
+        }
+
+        let anchor = match String::from_utf8(anchor) {
+            Ok(v) => v,
+            Err(_) => return Err(PfError::ConversionError),
+        };
+
+        let mut name = t.pfrt_name.to_vec();
+        while name.len() > 0 && name[name.len() - 1] == 0 {
+            name.pop();
+        }
+
+        let name = match String::from_utf8(name) {
+            Ok(v) => v,
+            Err(_) => return Err(PfError::ConversionError),
+        };
+
+        Ok(PfrTable { anchor, name })
+    }
+}
+
+impl TryInto<pfr_table> for PfrTable {
+    type Error = crate::PfError;
+    /// Will fail if anchor is PATH_MAX or greater, of if name is PF_TABLE_NAME_SIZE or greater
+    fn try_into(self) -> Result<pfr_table, PfError> {
+        if self.anchor.len() >= PATH_MAX 
+            || self.name.len() >= PF_TABLE_NAME_SIZE 
+        {
+            return Err(PfError::ConversionError);
+        }
+
+        let mut c_table = pfr_table::init();
+
+        for i in 0..self.anchor.len() {
+            c_table.pfrt_anchor[i] = self.anchor.as_bytes()[i];
+        }
+
+        for i in 0..self.name.len() {
+            c_table.pfrt_name[i] = self.name.as_bytes()[i];
+        }
+
+        Ok(c_table)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct PfiocTable {
+    pub table: PfrTable,
+    pub buffer: Vec<PfrAddr>,
+    // pub esize: i32, // len of pfr_addr... maybe impl a get_size() on PfrAddr?
+    // pub size: i32, // len of buffer can be infered
+    // Below fields are currently unused
+    // pub size2: i32,
+    // pub added: i32,
+    // pub deleted: i32,
+    // pub changed: i32,
+    // pub flags: i32,
+    // pub ticket: u32,
+}
+
+impl TryFrom<PfiocTableInter> for PfiocTable {
+    type Error = crate::PfError;
+
+    /// Will fail if PfrTable or PfrAddr conversions fail
+    fn try_from(io: PfiocTableInter) -> Result<PfiocTable, PfError> {
+        let PfiocTableInter { io, addrs } = io;
+
+        let mut addrs2: Vec<PfrAddr> = vec![];
+        for addr in addrs {
+            addrs2.push(addr.try_into()?);
+        }
+
+        Ok(PfiocTable {
+            table: io.pfrio_table.try_into()?,
+            buffer: addrs2,
+        })
+    }
+}
+
+impl TryInto<PfiocTableInter> for PfiocTable {
+    type Error = crate::PfError;
+    fn try_into(self) -> Result<PfiocTableInter, PfError> {
+        let mut addrs: Vec<pfr_addr> = vec![];
+        for addr in self.buffer {
+            addrs.push(addr.try_into()?);
+        }
+
+        let mut io = pfioc_table::init();
+        io.pfrio_table = self.table.try_into()?;
+        io.pfrio_buffer = addrs.as_mut_ptr();
+        io.pfrio_esize = PFR_ADDR_SIZE as i32;
+        io.pfrio_size = addrs.len() as i32;
+
+        Ok(PfiocTableInter { io, addrs })
+    }
+}
+
+/// Intermediate type to retain ownership of pfrio_buffer upon conversion to/from pfioc_table
+pub struct PfiocTableInter {
+    pub io: pfioc_table,
+    pub addrs: Vec<pfr_addr>,
 }
--- src/bridge/tests.rs
+++ src/bridge/tests.rs
@@ -1,9 +1,11 @@
 use crate::bridge::*;
 use std::net::{IpAddr, Ipv4Addr};
 use std::convert::TryInto;
+use std::error::Error;
+use std::{mem, slice};
 
 #[test]
-fn convert_pfaddr_from_c() {
+fn convert_pfraddr_from_c() {
     let mut c_addr = pfr_addr::init();
     c_addr.pfra_af = AF_INET;
     c_addr.pfra_u._pfra_ip4addr = u32::from_le_bytes([127, 0, 0, 1]);
@@ -12,27 +14,27 @@ fn convert_pfaddr_from_c() {
         118, 105, 111, 48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
     ];
 
-    let pf_addr = PfAddr::try_from(c_addr).unwrap();
+    let pf_addr = PfrAddr::try_from(c_addr).unwrap();
     assert_eq!(pf_addr.addr, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)));
     assert_eq!(pf_addr.ifname, String::from("vio0"));
     assert_eq!(pf_addr.subnet, 32);
 }
 
 #[test]
-fn convert_pfaddr_from_c_fail_af() {
+fn convert_pfraddr_from_c_fail_af() {
     let c_addr = pfr_addr::init();
-    assert!(PfAddr::try_from(c_addr).is_err());
+    assert!(PfrAddr::try_from(c_addr).is_err());
 }
 
 #[test]
-fn convert_pfaddr_into_c() {
-    let pf_addr = PfAddr {
+fn convert_pfraddr_into_c() {
+    let pf_addr = PfrAddr {
         addr: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
         ifname: String::from("vio0"),
         subnet: 32,
     };
 
-    let c_addr: pfr_addr = PfAddr::try_into(pf_addr).unwrap();
+    let c_addr: pfr_addr = PfrAddr::try_into(pf_addr).unwrap();
     assert_eq!(c_addr.pfra_af, AF_INET);
     assert_eq!(
         unsafe { c_addr.pfra_u._pfra_ip4addr }, 
@@ -46,12 +48,97 @@ fn convert_pfaddr_into_c() {
 }
 
 #[test]
-fn convert_pfaddr_into_c_fail_len() {
-    let pf_addr = PfAddr {
+fn convert_pfraddr_into_c_fail_len() {
+    let pf_addr = PfrAddr {
         addr: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
         ifname: String::from_utf8(vec![61u8; 16]).unwrap(),
         subnet: 32,
     };
 
     assert!(TryInto::<pfr_addr>::try_into(pf_addr).is_err());
+}
+
+#[test]
+fn convert_pfrtable_from_c() {
+    let mut c_table = pfr_table::init();
+    c_table.pfrt_anchor = [0; PATH_MAX];
+    c_table.pfrt_name = [
+        104, 101, 108, 108, 111, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    
+    let table = PfrTable::try_from(c_table).unwrap();
+    assert_eq!(table, PfrTable {
+        anchor: String::from(""),
+        name: String::from("hello"),
+    });
+}
+
+#[test]
+fn convert_pfrtable_into_c() {
+    let table = PfrTable {
+        anchor: String::from(""),
+        name: String::from("hello"),
+    };
+
+    let pfrt = TryInto::<pfr_table>::try_into(table).unwrap();
+
+    assert_eq!(pfrt.pfrt_name, [
+        104, 101, 108, 108, 111, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+}
+
+#[test]
+fn convert_pfrtable_from_c_invalid_utf8() {
+    let mut c_table = pfr_table::init();
+    c_table.pfrt_name = [
+        0, 159, 146, 150, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    
+    assert!(PfrTable::try_from(c_table).is_err());
+}
+
+#[test]
+fn convert_pfioctable_into_c() -> Result<(), Box<dyn Error>> {
+    let io = PfiocTable {
+        table: PfrTable {
+            name: String::from("my_table"),
+            anchor: String::from(""),
+        },
+        buffer: vec![
+            PfrAddr { 
+                ifname: String::from("vio0"), 
+                addr: IpAddr::V4("127.0.0.1".parse()?),
+                subnet: 32,
+            },
+        ],
+    };
+
+    let PfiocTableInter { io, addrs } = io.try_into()?;
+
+    // addrs and addrs2 will be one and the same. Don't forget to 'forget'!
+    let addrs2 = unsafe { slice::from_raw_parts(io.pfrio_buffer, addrs.len()) };
+
+    assert_eq!(addrs[0].pfra_net, addrs2[0].pfra_net);
+    mem::forget(addrs2);
+
+    Ok(())
+}
+
+#[test]
+fn convert_pfioctable_from_c() -> Result<(), Box<dyn Error>> {
+    let mut addrs: Vec<pfr_addr> = vec![PfrAddr { 
+        ifname: String::from("vio0"), 
+        addr: IpAddr::V4("127.0.0.1".parse()?),
+        subnet: 32,
+    }.try_into()?];
+    let mut io = pfioc_table::init();
+    io.pfrio_table = pfr_table::init();
+    io.pfrio_buffer = addrs.as_mut_ptr();
+    io.pfrio_esize = PFR_ADDR_SIZE as i32;
+    io.pfrio_size = addrs.len() as i32;
+
+    let inter = PfiocTableInter { io, addrs };
+    let _io: PfiocTable = inter.try_into()?;
+
+    Ok(())
 }
